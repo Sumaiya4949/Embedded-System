5a088c56f48edee745168ba6a9f4c0c0c64fdcce
Sumaiya4949 <32925935+Sumaiya4949@users.noreply.github.com>
2018-07-02 14:33:47 +0600

    Add files via upload

---
+++ Motor Driver IC.ino
@@ -0,0 +1,14 @@
+void setup()
+{
+  pinMode(3,OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(5, OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(3, HIGH);
+  digitalWrite(4, HIGH);
+  digitalWrite(5, LOW);
+  
+}
